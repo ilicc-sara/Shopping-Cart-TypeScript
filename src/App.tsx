442cffa2a5b73a1d794b8a64ad48a01779073062
Sara <sarailic431@gmail.com>
2025-11-01 21:40:46 +0100

    tailwind and react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-red-400">Munem Mash</h1>
+    </>
+  );
 }
 
 export default App;
